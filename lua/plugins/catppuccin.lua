5c760c3e78bdea6769fdf779d94150b3b845a2d1
Preet Arora <141331002+Casper3004@users.noreply.github.com>
2026-04-12 16:26:05 +0530

    Update nvim-configuration

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,9 +1,8 @@
-
- return {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme("catppuccin")
-    end,
-  }
+return {
+	"lewpoly/sherbet.nvim",
+   name = "lewpoly",
+   config = function()
+		vim.opt.termguicolors = true
+		vim.cmd("colorscheme sherbet")
+	end,
+}
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,6 +3,10 @@ vim.cmd("set tabstop=3")
 vim.cmd("set softtabstop=3")
 vim.cmd("set shiftwidth=3")
 vim.opt.nu = true vim.opt.relativenumber = true vim.opt.wrap = true
+vim.keymap.set("n", "<leader>h", "<C-w>h")
+vim.keymap.set("n", "<leader>l", "<C-w>l")
+vim.keymap.set("n", "<leader>j", "<C-w>j")
+vim.keymap.set("n", "<leader>k", "<C-w>k")
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
@@ -14,6 +18,3 @@ vim.opt.lazyredraw = false
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
-vim.keymap.set("n", "<leader>f", function ()
-   vim.cmd("!npx prisma format")
-end)
